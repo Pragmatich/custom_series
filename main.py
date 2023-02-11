1f6b3872af3d27565b56dd9859f99333be0c52ea
Pragmatich <35996804+Pragmatich@users.noreply.github.com>
2023-02-11 14:54:34 +0100

    Working variant

--- main.py
+++ main.py
@@ -2,44 +2,48 @@ def custom_series():
     steps = 2
     step_sizes = []
     range_list = [1, 5, 0, 10]
-    final_list = []
-    
-    # Creat the step_sizes from ranger_list and steps
+    final_list = [[]]
+
     for i in range(0, len(range_list), 2):
         step_sizes.append(int((range_list[i+1] - range_list[i]) / steps))
-        final_list.append(range_list[i])
-        final_list.append(range_list[i] + step_sizes[int(i/2)])
-    
-    # reamining_enter --> to define how much level necessary
+        final_list[0].append(range_list[i])
+        final_list[0].append(range_list[i] + step_sizes[int(i/2)])
+
     remaining_enter = len(range_list) - 1
-    final_list_pre = final_list.copy()
+    final_list_2 = final_list[0].copy()
 
-    print(step_sizes)
-
-    def range_stepper(remaining_enter, final_list_pre):
+    def range_stepper(remaining_enter, final_list_2):
         i = len(range_list) - remaining_enter - 1
-        print(f'remaining_enter: {remaining_enter}')
 
-        # If i odd
+        # If i is odd
         if i % 2:
             step = step_sizes[int((i-1)/2)]
-            from_range = final_list_pre[i-1] + step
+            from_range = final_list_2[i-1] + step
             to_range = range_list[i] + step
         
-        # If i even
+        # If i is even
         else:
             step = step_sizes[int(i/2)]
             from_range = range_list[i]
-            to_range = final_list_pre[i+1]
-    
+            to_range = range_list[i+1]
+
+        final_list_0 = []
 
         for j in range(from_range, to_range, step):
+            final_list_2[i] = j
+            final_list_1 = []
+
             if remaining_enter:
-                range_stepper(remaining_enter - 1, final_list_pre)
-            final_list_pre[i] = j
-            print(f'j: {j}')
-            final_list.append(final_list_pre)
-            
-    range_stepper(remaining_enter, final_list_pre)
+                final_list_1 = range_stepper(remaining_enter - 1, final_list_2).copy()
+                for element in final_list_1:
+                    final_list_0.append(element.copy())
+            else:
+                final_list_0.append(final_list_2.copy())
+
+        return final_list_0
+
+    final_list = range_stepper(remaining_enter, final_list_2).copy()
+
+    print(final_list)
 
 custom_series()
